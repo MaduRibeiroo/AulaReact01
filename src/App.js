d97bf516f2633d38802337f81fd09fa3118469be
MaduRibeiroo <maadu.ribeiro1404@gmail.com>
2024-08-26 10:12:36 -0300

    Resolvendo o exercicio e conhecendo a sitaxe JSX

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Data from "./meusComponentes/Data";
+import Contador from "./meusComponentes/ContadorV2"
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
         <Data texto="Data e hora atuais: " timeZone={-3.00}/>
         <Data texto="Data e hora em Londres: " timeZone={1.00}/>
         <Data texto="Data e hora em Nova York:" timeZone={-4.00}/>
+        <Contador />
     </div>
   );
 }
